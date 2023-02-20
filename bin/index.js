2640b3f90fc09114ed7901574ea84486ff19d60d
Ubaid196 <uurrehmansiddiqui@gmail.com>
2023-02-20 20:27:11 +0500

    To Do TS Project

---
+++ bin/index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import showBanner from "node-banner";
+async function myBanner() {
+    await showBanner("\nToDo App", chalk.blue("Welcome!"), "green");
+}
+await myBanner();
+async function toDos() {
+    let isValid = true;
+    var to_Dos = [];
+    while (isValid) {
+        let toDoList = await inquirer.prompt([
+            {
+                name: "yourToDos",
+                type: "string",
+                message: "Enter your to Dos: ",
+            },
+        ]);
+        const { yourToDos } = toDoList;
+        if (yourToDos) {
+            to_Dos.push(yourToDos);
+            isValid = true;
+        }
+        else {
+            console.log(`Your To Do List`);
+            for (let i = 0; i < to_Dos.length; i++) {
+                console.log(to_Dos[i]);
+            }
+            isValid = false;
+            break;
+        }
+    }
+    return to_Dos;
+}
+let myToDoList = toDos();
